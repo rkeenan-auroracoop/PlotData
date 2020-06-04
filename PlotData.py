691dfc9b110c146e430758205f56bdb0ebca45a6
Ross Keenan <rkeenan@auroracoop.com>
2020-06-04 13:02:25 -0500

    Added Notes & Harvest forms functions

--- PlotData.py
+++ PlotData.py
@@ -1,81 +1,279 @@
 import openpyxl
 import pprint
 
+# This is for the PLANTING FORM
+
 wb = openpyxl.load_workbook(
     r'C:\Users\rkeenan\OneDrive - Aurora Cooperative\Documents\2020 Tableau Updates\Al Perry\2020 Aaron Franson Test Plot.xlsx')
-sheet = wb['PLANTING FORM']
-writeFile = open(
-    r'C:\Users\rkeenan\OneDrive - Aurora Cooperative\Documents\2020 Tableau Updates\Al Perry\2020 Aaron Franson Test Plot.txt', 'w')
+plantingSheet = wb['PLANTING FORM']
+plantingWriteFile = open(
+    r'C:\Users\rkeenan\OneDrive - Aurora Cooperative\Documents\2020 Tableau Updates\Al Perry\Test Plot(PLANTING).txt', 'w')
 
 
 def topPlantingInfo():
-    GROWER_NAME = sheet['C3'].value
-    GROWER_CITY = sheet['C4'].value
-    COUNTY = sheet['C5'].value
-    ACE_LOCATION = sheet['C6'].value
-    STATED_PLOT_ON = sheet['C7'].value
-    FLAT_LOCATION = sheet['C8'].value
-    GPS_LATITUDE = sheet['C9'].value
-    FUNGICIDE = sheet['C10'].value
+    GROWER_NAME = plantingSheet['C3'].value
+    GROWER_CITY = plantingSheet['C4'].value
+    COUNTY = plantingSheet['C5'].value
+    ACE_LOCATION = plantingSheet['C6'].value
+    STATED_PLOT_ON = plantingSheet['C7'].value
+    FLAT_LOCATION = plantingSheet['C8'].value
+    GPS_LATITUDE = plantingSheet['C9'].value
+    FUNGICIDE = plantingSheet['C10'].value
     if FUNGICIDE != None:
         FUNGICIDE = FUNGICIDE
     else:
         FUNGICIDE = "None"
-    CROP = sheet['H3'].value
-    PLANTING_DATE = sheet['H4'].value
-    SEEDING_RATE = sheet['H5'].value
-    PLANTING_DEPTH_IN = sheet['H6'].value
-    PLANTER_TYPE = sheet['H7'].value
-    ROW_WIDTH = sheet['H8'].value
-    GPS_LONGITUDE = sheet['H9'].value
-    HERBICIDE = sheet['H10'].value
+    CROP = plantingSheet['H3'].value
+    PLANTING_DATE = plantingSheet['H4'].value
+    SEEDING_RATE = plantingSheet['H5'].value
+    PLANTING_DEPTH_IN = plantingSheet['H6'].value
+    PLANTER_TYPE = plantingSheet['H7'].value
+    ROW_WIDTH = plantingSheet['H8'].value
+    GPS_LONGITUDE = plantingSheet['H9'].value
+    HERBICIDE = plantingSheet['H10'].value
     if HERBICIDE != None:
         HERBICIDE = HERBICIDE
     else:
         HERBICIDE = "None"
-    PLOT_TYPE = sheet['L5'].value
-    IRRIGATION_TYPE = sheet['L6'].value
-    PREVIOUS_CROP = sheet['L7'].value
-    TILLAGE_SYSTEM = sheet['L8'].value
-    SOIL_TEXTURE = sheet['L9'].value
-    INSECTICIDE_RATE = sheet['L10'].value
+    PLOT_TYPE = plantingSheet['L5'].value
+    IRRIGATION_TYPE = plantingSheet['L6'].value
+    PREVIOUS_CROP = plantingSheet['L7'].value
+    TILLAGE_SYSTEM = plantingSheet['L8'].value
+    SOIL_TEXTURE = plantingSheet['L9'].value
+    INSECTICIDE_RATE = plantingSheet['L10'].value
     if INSECTICIDE_RATE != None:
         INSECTICIDE_RATE = INSECTICIDE_RATE
     else:
         INSECTICIDE_RATE = "None"
     FORM_TYPE = "PLANTING FORM"
 
-    writeFile.write(GROWER_NAME + "\t" + GROWER_CITY + "\t" + COUNTY + "\t" + ACE_LOCATION + "\t" + STATED_PLOT_ON + "\t" + FLAT_LOCATION + "\t" + str(GPS_LATITUDE) + "\t" + FUNGICIDE + "\t" + CROP + "\t" + str(PLANTING_DATE) + "\t" + str(SEEDING_RATE) + "\t" +
-                    str(PLANTING_DEPTH_IN) + "\t" + PLANTER_TYPE + "\t" + str(ROW_WIDTH) + "\t" + str(GPS_LONGITUDE) + "\t" + HERBICIDE + "\t" + PLOT_TYPE + "\t" + IRRIGATION_TYPE + "\t" + PREVIOUS_CROP + "\t" + TILLAGE_SYSTEM + "\t" + SOIL_TEXTURE + "\t" + INSECTICIDE_RATE + "\t" + FORM_TYPE + "\t")
+    plantingWriteFile.write(GROWER_NAME + "\t" + GROWER_CITY + "\t" + COUNTY + "\t" + ACE_LOCATION + "\t" + STATED_PLOT_ON + "\t" + FLAT_LOCATION + "\t" + str(GPS_LATITUDE) + "\t" + FUNGICIDE + "\t" + CROP + "\t" + str(PLANTING_DATE) + "\t" + str(SEEDING_RATE) + "\t" +
+                            str(PLANTING_DEPTH_IN) + "\t" + PLANTER_TYPE + "\t" + str(ROW_WIDTH) + "\t" + str(GPS_LONGITUDE) + "\t" + HERBICIDE + "\t" + PLOT_TYPE + "\t" + IRRIGATION_TYPE + "\t" + PREVIOUS_CROP + "\t" + TILLAGE_SYSTEM + "\t" + SOIL_TEXTURE + "\t" + INSECTICIDE_RATE + "\t" + FORM_TYPE + "\t")
 
 
 def bottomPlantingInfo():
-    for row in range(17, sheet.max_row + 1):
-        ENTRY = sheet['A' + str(row)].value
-        COMPANY = sheet['C' + str(row)].value
-        HYBRID_VARIETY = sheet['F' + str(row)].value
-        SEED_TREATMENTS = sheet['J' + str(row)].value
+    for row in range(17, plantingSheet.max_row + 1):
+        ENTRY = plantingSheet['A' + str(row)].value
+        COMPANY = plantingSheet['C' + str(row)].value
+        HYBRID_VARIETY = plantingSheet['F' + str(row)].value
+        SEED_TREATMENTS = plantingSheet['J' + str(row)].value
         if SEED_TREATMENTS != None:
             SEED_TREATMENTS = SEED_TREATMENTS
         else:
             SEED_TREATMENTS = "None"
-        NUM_OF_ROWS = sheet['M' + str(row)].value
+        NUM_OF_ROWS = plantingSheet['M' + str(row)].value
 
         if COMPANY != None and HYBRID_VARIETY != None:
             topPlantingInfo()
-            writeFile.write(str(ENTRY) + "\t" + COMPANY + "\t" + str(HYBRID_VARIETY) +
-                            "\t" + SEED_TREATMENTS + "\t" + str(NUM_OF_ROWS) + "\n")
+            plantingWriteFile.write(str(ENTRY) + "\t" + COMPANY + "\t" + str(HYBRID_VARIETY) +
+                                    "\t" + SEED_TREATMENTS + "\t" + str(NUM_OF_ROWS) + "\n")
         else:
             continue
-
-# def topNotesInfo():
-
-# def bottomNotesInfo():
-
-# def topHarvestInfo():
-
-# def bottomHarvestInfo():
+    print("Your Planting Form data plot file is done!")
 
 
 bottomPlantingInfo()
-writeFile.close()
+plantingWriteFile.close()
+wb.close()
+
+# This is for the NOTES FORM
+
+wb = openpyxl.load_workbook(
+    r'C:\Users\rkeenan\OneDrive - Aurora Cooperative\Documents\2020 Tableau Updates\Al Perry\2020 Aaron Franson Test Plot.xlsx')
+notesSheet = wb['NOTES FORM']
+notesWriteFile = open(
+    r'C:\Users\rkeenan\OneDrive - Aurora Cooperative\Documents\2020 Tableau Updates\Al Perry\Test Plot(NOTES).txt', 'w')
+
+
+def topNotesInfo():
+    GROWER_NAME = notesSheet['C3'].value
+    GROWER_CITY = notesSheet['C4'].value
+    COUNTY = notesSheet['C5'].value
+    ACE_LOCATION = notesSheet['C6'].value
+    STATED_PLOT_ON = notesSheet['C7'].value
+    FLAT_LOCATION = notesSheet['C8'].value
+    GPS_LATITUDE = notesSheet['C9'].value
+    FUNGICIDE = notesSheet['C10'].value
+    if FUNGICIDE != None:
+        FUNGICIDE = FUNGICIDE
+    else:
+        FUNGICIDE = "None"
+    CROP = notesSheet['H3'].value
+    PLANTING_DATE = notesSheet['H4'].value
+    SEEDING_RATE = notesSheet['H5'].value
+    PLANTING_DEPTH_IN = notesSheet['H6'].value
+    PLANTER_TYPE = notesSheet['H7'].value
+    ROW_WIDTH = notesSheet['H8'].value
+    GPS_LONGITUDE = notesSheet['H9'].value
+    HERBICIDE = notesSheet['H10'].value
+    if HERBICIDE != None:
+        HERBICIDE = HERBICIDE
+    else:
+        HERBICIDE = "None"
+    PLOT_TYPE = notesSheet['L5'].value
+    IRRIGATION_TYPE = notesSheet['L6'].value
+    PREVIOUS_CROP = notesSheet['L7'].value
+    TILLAGE_SYSTEM = notesSheet['L8'].value
+    SOIL_TEXTURE = notesSheet['L9'].value
+    INSECTICIDE_RATE = notesSheet['L10'].value
+    if INSECTICIDE_RATE != None:
+        INSECTICIDE_RATE = INSECTICIDE_RATE
+    else:
+        INSECTICIDE_RATE = "None"
+    FORM_TYPE = "NOTES FORM"
+
+    notesWriteFile.write(GROWER_NAME + "\t" + GROWER_CITY + "\t" + COUNTY + "\t" + ACE_LOCATION + "\t" + STATED_PLOT_ON + "\t" + FLAT_LOCATION + "\t" + str(GPS_LATITUDE) + "\t" + FUNGICIDE + "\t" + CROP + "\t" + str(PLANTING_DATE) + "\t" + str(SEEDING_RATE) + "\t" +
+                         str(PLANTING_DEPTH_IN) + "\t" + PLANTER_TYPE + "\t" + str(ROW_WIDTH) + "\t" + str(GPS_LONGITUDE) + "\t" + HERBICIDE + "\t" + PLOT_TYPE + "\t" + IRRIGATION_TYPE + "\t" + PREVIOUS_CROP + "\t" + TILLAGE_SYSTEM + "\t" + SOIL_TEXTURE + "\t" + INSECTICIDE_RATE + "\t" + FORM_TYPE + "\t")
+
+
+def bottomNotesInfo():
+    for row in range(15, notesSheet.max_row + 1):
+        HYBRID_VARIETY = notesSheet['A' + str(row)].value
+        EMERGENCE = notesSheet['C' + str(row)].value
+        if EMERGENCE != None:
+            EMERGENCE = EMERGENCE
+        else:
+            EMERGENCE = "None"
+        EARLY_VIGOR = notesSheet['D' + str(row)].value
+        if EARLY_VIGOR != None:
+            EARLY_VIGOR = EARLY_VIGOR
+        else:
+            EARLY_VIGOR = "None"
+        LATE_PLANT_HEALTH = notesSheet['E' + str(row)].value
+        if LATE_PLANT_HEALTH != None:
+            LATE_PLANT_HEALTH = LATE_PLANT_HEALTH
+        else:
+            LATE_PLANT_HEALTH = "None"
+        GENERAL_COMMENTS = notesSheet['F' + str(row)].value
+        if GENERAL_COMMENTS != None:
+            GENERAL_COMMENTS = GENERAL_COMMENTS
+        else:
+            GENERAL_COMMENTS = "None"
+        ENTRY = notesSheet['M' + str(row)].value
+
+        if HYBRID_VARIETY != None and EMERGENCE != None:
+            topNotesInfo()
+            notesWriteFile.write(str(HYBRID_VARIETY) + "\t" + str(EMERGENCE) + "\t" + str(EARLY_VIGOR) +
+                                 "\t" + str(LATE_PLANT_HEALTH) + "\t" + str(GENERAL_COMMENTS) + "\t" + str(GENERAL_COMMENTS) + "\n")
+        else:
+            continue
+
+    print("Your Notes Form data plot file is done!")
+
+
+bottomNotesInfo()
+notesWriteFile.close()
+
+
+# This is for the HARVEST FORM
+
+harvestSheet = wb['HARVEST FORM']
+harvestWriteFile = open(
+    r'C:\Users\rkeenan\OneDrive - Aurora Cooperative\Documents\2020 Tableau Updates\Al Perry\Test Plot(HARVEST).txt', 'w')
+
+
+def topHarvestInfo():
+    GROWER_NAME = harvestSheet['C3'].value
+    GROWER_CITY = harvestSheet['C4'].value
+    COUNTY = harvestSheet['C5'].value
+    ACE_LOCATION = harvestSheet['C6'].value
+    STATED_PLOT_ON = harvestSheet['C7'].value
+    FLAT_LOCATION = harvestSheet['C8'].value
+    GPS_LATITUDE = harvestSheet['C9'].value
+    FUNGICIDE = harvestSheet['C10'].value
+    if FUNGICIDE != None:
+        FUNGICIDE = FUNGICIDE
+    else:
+        FUNGICIDE = "None"
+    HARVEST_DATE = harvestSheet['C11'].value
+    if HARVEST_DATE != None:
+        HARVEST_DATE = HARVEST_DATE
+    else:
+        HARVEST_DATE = "None"
+    CROP = harvestSheet['H3'].value
+    PLANTING_DATE = harvestSheet['H4'].value
+    SEEDING_RATE = harvestSheet['H5'].value
+    PLANTING_DEPTH_IN = harvestSheet['H6'].value
+    PLANTER_TYPE = harvestSheet['H7'].value
+    ROW_WIDTH = harvestSheet['H8'].value
+    GPS_LONGITUDE = harvestSheet['H9'].value
+    HERBICIDE = harvestSheet['H10'].value
+    if HERBICIDE != None:
+        HERBICIDE = HERBICIDE
+    else:
+        HERBICIDE = "None"
+    COMMODITY_PRICE = harvestSheet['H11'].value
+    PLOT_TYPE = harvestSheet['L5'].value
+    IRRIGATION_TYPE = harvestSheet['L6'].value
+    PREVIOUS_CROP = harvestSheet['L7'].value
+    TILLAGE_SYSTEM = harvestSheet['L8'].value
+    SOIL_TEXTURE = harvestSheet['L9'].value
+    INSECTICIDE_RATE = harvestSheet['L10'].value
+    if INSECTICIDE_RATE != None:
+        INSECTICIDE_RATE = INSECTICIDE_RATE
+    else:
+        INSECTICIDE_RATE = "None"
+    DRYING_COST = harvestSheet['L11'].value
+    FORM_TYPE = "HARVEST FORM"
+
+    harvestWriteFile.write(GROWER_NAME + "\t" + GROWER_CITY + "\t" + COUNTY + "\t" + ACE_LOCATION + "\t" + STATED_PLOT_ON + "\t" + FLAT_LOCATION + "\t" + str(GPS_LATITUDE) + "\t" + FUNGICIDE + "\t" + HARVEST_DATE + "\t" + CROP + "\t" + str(PLANTING_DATE) + "\t" + str(SEEDING_RATE) + "\t" +
+                           str(PLANTING_DEPTH_IN) + "\t" + PLANTER_TYPE + "\t" + str(ROW_WIDTH) + "\t" + str(GPS_LONGITUDE) + "\t" + HERBICIDE + "\t" + str(COMMODITY_PRICE) + "\t" + PLOT_TYPE + "\t" + IRRIGATION_TYPE + "\t" + PREVIOUS_CROP + "\t" + TILLAGE_SYSTEM + "\t" + SOIL_TEXTURE + "\t" + INSECTICIDE_RATE + "\t" + str(DRYING_COST) + "\t" + FORM_TYPE + "\t")
+
+
+def bottomHarvestInfo():
+    for row in range(17, harvestSheet.max_row + 1):
+        BRAND = harvestSheet['A' + str(row)].value
+        PRODUCT = harvestSheet['C' + str(row)].value
+        ROW_LENGTH = harvestSheet['F' + str(row)].value
+        if ROW_LENGTH != None:
+            ROW_LENGTH = ROW_LENGTH
+        else:
+            ROW_LENGTH = "None"
+        WET_WEIGHT = harvestSheet['G' + str(row)].value
+        if WET_WEIGHT != None:
+            WET_WEIGHT = WET_WEIGHT
+        else:
+            WET_WEIGHT = "None"
+        HARVEST_MOISTURE_PCT = harvestSheet['H' + str(row)].value
+        if HARVEST_MOISTURE_PCT != None:
+            HARVEST_MOISTURE_PCT = HARVEST_MOISTURE_PCT
+        else:
+            HARVEST_MOISTURE_PCT = "None"
+        NUM_OF_ROWS = harvestSheet['I' + str(row)].value
+        TEST_WEIGHT = harvestSheet['J' + str(row)].value
+        if TEST_WEIGHT != None:
+            TEST_WEIGHT = TEST_WEIGHT
+        else:
+            TEST_WEIGHT = "None"
+        YIELD_PER_ACRE = harvestSheet['K' + str(row)].value
+        if YIELD_PER_ACRE != None:
+            YIELD_PER_ACRE = YIELD_PER_ACRE
+        else:
+            YIELD_PER_ACRE = "None"
+        PCT_OF_PLOT_ADVANTAGE = harvestSheet['L' + str(row)].value
+        if PCT_OF_PLOT_ADVANTAGE != None:
+            PCT_OF_PLOT_ADVANTAGE = PCT_OF_PLOT_ADVANTAGE
+        else:
+            PCT_OF_PLOT_ADVANTAGE = "None"
+        YIELD_PER_ACRE_RANK = harvestSheet['M' + str(row)].value
+        if YIELD_PER_ACRE_RANK != None:
+            YIELD_PER_ACRE_RANK = YIELD_PER_ACRE_RANK
+        else:
+            YIELD_PER_ACRE_RANK = "None"
+        DOLLARS_PER_ACRE_RANK = harvestSheet['N' + str(row)].value
+        if DOLLARS_PER_ACRE_RANK != None:
+            DOLLARS_PER_ACRE_RANK = DOLLARS_PER_ACRE_RANK
+        else:
+            DOLLARS_PER_ACRE_RANK = "None"
+        if BRAND != None and PRODUCT != None:
+            topHarvestInfo()
+            harvestWriteFile.write(BRAND + "\t" + str(PRODUCT) + "\t" + str(ROW_LENGTH) +
+                                   "\t" + str(WET_WEIGHT) + "\t" + str(HARVEST_MOISTURE_PCT) + "\t" + str(NUM_OF_ROWS) + "\t" + str(TEST_WEIGHT) + "\t" + str(YIELD_PER_ACRE) + "\t" + str(PCT_OF_PLOT_ADVANTAGE) + "\t" + str(YIELD_PER_ACRE_RANK) + "\t" + str(DOLLARS_PER_ACRE_RANK) + "\n")
+        else:
+            continue
+
+    print("Your Harvest Form data plot file is done!")
+
+
+bottomHarvestInfo()
+harvestWriteFile.close()
